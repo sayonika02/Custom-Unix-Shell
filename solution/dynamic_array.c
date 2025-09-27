7f90f7ea445323298749b4931e163a4516a175ba
Sayonika Das <das76@wisc.edu>
2025-09-26 23:35:30 -0500

    initial commit - needs more work

--- solution/dynamic_array.c
+++ solution/dynamic_array.c
@@ -1,6 +1,83 @@
 #include "dynamic_array.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+DynamicArray* da_create(size_t init_capacity) {
+    DynamicArray *da = malloc(sizeof(DynamicArray));
+    if (!da) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    da->data = malloc(init_capacity * sizeof(char*));
+    if (!da->data) {
+        perror("malloc");
+        free(da);
+        exit(EXIT_FAILURE);
+    }
+    da->size = 0;
+    da->capacity = init_capacity;
+    return da;
+}
 
-/** 
-* TODO: Implement all the methods with declarations in dynamic_array.h here, before proceeding with your project
-*/
+void da_put(DynamicArray *da, const char* val) {
+    if (!da || !val) return;
+
+    // Resize if the array is full
+    if (da->size >= da->capacity) {
+        size_t new_capacity = da->capacity == 0 ? 1 : da->capacity * 2;
+        char **new_data = realloc(da->data, new_capacity * sizeof(char*));
+        if (!new_data) {
+            perror("realloc");
+            // In a real-world app, you might not want to exit here, but for this project it's fine.
+            da_free(da);
+            exit(EXIT_FAILURE);
+        }
+        da->data = new_data;
+        da->capacity = new_capacity;
+    }
+
+    // Add the new element (create a copy)
+    da->data[da->size] = strdup(val);
+    if (!da->data[da->size]) {
+        perror("strdup");
+        da_free(da);
+        exit(EXIT_FAILURE);
+    }
+    da->size++;
+}
+
+char *da_get(DynamicArray *da, const size_t ind) {
+    if (!da || ind >= da->size) {
+        return NULL;
+    }
+    return da->data[ind];
+}
+
+void da_delete(DynamicArray *da, const size_t ind) {
+    if (!da || ind >= da->size) {
+        return;
+    }
+    free(da->data[ind]); // Free the string itself
+    // Shift elements to the left to fill the gap
+    for (size_t i = ind; i < da->size - 1; i++) {
+        da->data[i] = da->data[i + 1];
+    }
+    da->size--;
+}
+
+void da_print(DynamicArray *da) {
+    if (!da) return;
+    for (size_t i = 0; i < da->size; i++) {
+        printf("%s\n", da->data[i]);
+    }
+}
+
+void da_free(DynamicArray *da) {
+    if (!da) return;
+    for (size_t i = 0; i < da->size; i++) {
+        free(da->data[i]);
+    }
+    free(da->data);
+    free(da);
+}
--- solution/wsh.c
+++ solution/wsh.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE //strdup, strtok_r
 #include "wsh.h"
 #include "dynamic_array.h"
 #include "utils.h"
@@ -8,9 +9,20 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <ctype.h>
 
 int rc;
 HashMap *alias_hm = NULL;
+DynamicArray *history = NULL;
+FILE *batch_file = NULL;
+
+// Forward declarations
+void execute_line(char *line);
+int handle_builtin(int argc, char **argv);
+char* find_executable(const char *cmd);
+void free_argv(int argc, char **argv);
 
 /***************************************************
  * Helper Functions
@@ -20,8 +32,8 @@ HashMap *alias_hm = NULL;
  */
 void wsh_free(void)
 {
-  // Free any allocated resources here
   if (alias_hm != NULL) { hm_free(alias_hm); alias_hm = NULL; }
+  if (history != NULL) { da_free(history); history = NULL; }
 }
 
 /**
@@ -45,7 +57,6 @@ void wsh_warn(const char *msg, ...)
 {
   va_list args;
   va_start(args, msg);
-
   vfprintf(stderr, msg, args);
   va_end(args);
   rc = EXIT_FAILURE;
@@ -61,22 +72,20 @@ void wsh_warn(const char *msg, ...)
 int main(int argc, char **argv)
 {
   alias_hm = hm_create();
+  history = da_create(16);
   setenv("PATH", "/bin", 1);
+  rc = EXIT_SUCCESS;
+
   if (argc > 2)
   {
     wsh_warn(INVALID_WSH_USE);
-    return EXIT_FAILURE;
+    clean_exit(EXIT_FAILURE);
   }
-  switch (argc)
-  {
-    case 1:
-      interactive_main();
-      break;
-    case 2:
-      rc = batch_main(argv[1]);
-      break;
-    default:
-      break;
+  
+  if (argc == 1) {
+    interactive_main();
+  } else {
+    rc = batch_main(argv[1]);
   }
   wsh_free();
   return rc;
@@ -90,34 +99,362 @@ int main(int argc, char **argv)
  * @Brief Interactive mode: print prompt and wait for user input
  * execute the given input and repeat
  */
+// Replace BOTH of your main loop functions with these
+
 void interactive_main(void)
 {
-  // TODO: Implement interactive mode here
-}
+  char line[MAX_LINE];
+  while (1) {
+    printf(PROMPT);
+    fflush(stdout);
 
-/**
- * @Brief Batch mode: read commands from script file line by line
- * execute each command and repeat until EOF
- *
- * @param script_file Path to the script file
- * @return EXIT_SUCCESS(0) on success, EXIT_FAILURE(1) on error
- */
+    if (fgets(line, MAX_LINE, stdin) == NULL) {
+      if (ferror(stdin)) {
+        perror("fgets");
+      }
+      break; // EOF
+    }
+    
+    line[strcspn(line, "\n")] = 0;
+
+    char* p = line;
+    while (*p && isspace(*p)) p++;
+    if (*p == '\0') continue;
+    
+    da_put(history, line);
+    execute_line(line);
+  }
+  clean_exit(rc);
+}
 
 int batch_main(const char *script_file)
 {
-  // TODO: Implement batch mode here
-  printf("%s\n",script_file);
-  return 0;
+  batch_file = fopen(script_file, "r");
+  if (!batch_file) {
+    perror("fopen");
+    return EXIT_FAILURE;
+  }
+
+  char line[MAX_LINE];
+  while (fgets(line, MAX_LINE, batch_file) != NULL) { 
+    line[strcspn(line, "\n")] = 0;
+
+    char* p = line;
+    while (*p && isspace(*p)) p++;
+    if (*p == '\0') continue;
+    
+    da_put(history, line);
+    execute_line(line);
+  }
+
+  fclose(batch_file); 
+  return rc;
 }
 
-/***************************************************
- * Parsing
- ***************************************************/
+void execute_single_command(char *command_str) {
+    if (batch_file != NULL) {
+        fclose(batch_file);
+    }
+    rc = EXIT_SUCCESS;
+  
+    int argc = 0;
+    char *argv[MAX_ARGS];
+
+    char *trimmed_cmd = strdup(command_str);
+    char *first_word_end = strchr(trimmed_cmd, ' ');
+    if (first_word_end) *first_word_end = '\0';
+    
+    char* alias_val = hm_get(alias_hm, trimmed_cmd);
+    char* final_cmd = NULL;
+
+    if (alias_val) {
+        final_cmd = strdup(alias_val);
+        if (first_word_end) {
+            final_cmd = append(final_cmd, first_word_end + 1);
+        }
+    } else {
+        final_cmd = strdup(command_str);
+    }
+    free(trimmed_cmd);
+
+    parseline_no_subst(final_cmd, argv, &argc);
+    free(final_cmd);
+    
+    if (argc == 0) {
+        clean_exit(rc);
+    }
+    
+    if (handle_builtin(argc, argv)) {
+        free_argv(argc, argv);
+        clean_exit(rc);
+    }
+    
+    char* exec_path = find_executable(argv[0]);
+    if (!exec_path) {
+        wsh_warn(CMD_NOT_FOUND, argv[0]);
+        free_argv(argc, argv);
+        clean_exit(EXIT_FAILURE);
+    }
+
+    execv(exec_path, argv);
+    
+    perror("execv");
+    free(exec_path);
+    free_argv(argc, argv);
+    clean_exit(EXIT_FAILURE);
+}
+
+void execute_line(char *line) {
+    char *pipe_segments[MAX_ARGS];
+    int num_segments = 0;
+    char *line_copy = strdup(line);
+    if (!line_copy) { perror("strdup"); return; }
+    char *saveptr;
+
+    char *token = strtok_r(line_copy, "|", &saveptr);
+    while (token != NULL && num_segments < MAX_ARGS) {
+        pipe_segments[num_segments++] = token;
+        token = strtok_r(NULL, "|", &saveptr);
+    }
+    
+    if (num_segments == 0) {
+        free(line_copy);
+        return;
+    }
+    for (int i = 0; i < num_segments; i++) {
+        char *trimmed = pipe_segments[i];
+        while (*trimmed && isspace(*trimmed)) trimmed++;
+        if (*trimmed == '\0') {
+            wsh_warn(EMPTY_PIPE_SEGMENT);
+            free(line_copy);
+            return;
+        }
+    }
+
+    if (num_segments == 1) {
+        int argc;
+        char *argv[MAX_ARGS];
+        parseline_no_subst(pipe_segments[0], argv, &argc);
+
+        if (argc > 0) {
+            if (!handle_builtin(argc, argv)) {
+                pid_t pid = fork();
+                if (pid == -1) {
+                    perror("fork");
+                } else if (pid == 0) { 
+                    execute_single_command(pipe_segments[0]);
+                } else {
+                    int status;
+                    waitpid(pid, &status, 0);
+                    if (WIFEXITED(status)) {
+                        rc = WEXITSTATUS(status);
+                    }
+                }
+            }
+        }
+        free_argv(argc, argv);
+        free(line_copy);
+        return;
+    }
+
+    pid_t pids[num_segments];
+    int prev_pipe_fd = -1; 
+    int pipe_fds[2];
+
+    for (int i = 0; i < num_segments; i++) {
+        if (i < num_segments - 1) {
+            if (pipe(pipe_fds) == -1) { perror("pipe"); free(line_copy); return; }
+        }
+
+        pids[i] = fork();
+        if (pids[i] == -1) { perror("fork"); break; }
+
+        if (pids[i] == 0) { 
+            if (prev_pipe_fd != -1) {
+                dup2(prev_pipe_fd, STDIN_FILENO);
+                close(prev_pipe_fd);
+            }
+            if (i < num_segments - 1) {
+                close(pipe_fds[0]);
+                dup2(pipe_fds[1], STDOUT_FILENO);
+                close(pipe_fds[1]);
+            }
+            execute_single_command(pipe_segments[i]);
+        } else { 
+            if (prev_pipe_fd != -1) {
+                close(prev_pipe_fd);
+            }
+            if (i < num_segments - 1) {
+                close(pipe_fds[1]);
+                prev_pipe_fd = pipe_fds[0];
+            }
+        }
+    }
+    
+    free(line_copy);
+
+    for (int i = 0; i < num_segments; i++) {
+        int status;
+        waitpid(pids[i], &status, 0);
+        if (WIFEXITED(status)) {
+            rc = WEXITSTATUS(status);
+        }
+    }
+}
+
+int handle_builtin(int argc, char **argv) {
+    if (strcmp(argv[0], "exit") == 0) {
+        if (argc > 1) { 
+            wsh_warn(INVALID_EXIT_USE); 
+            return 1;
+        }
+        clean_exit(rc);
+    }
+    else if (strcmp(argv[0], "cd") == 0) {
+        if (argc > 2) { wsh_warn(INVALID_CD_USE); return 1; }
+        char *dir;
+        if (argc == 1) {
+            dir = getenv("HOME");
+            if (dir == NULL) { wsh_warn(CD_NO_HOME); return 1; }
+        } else {
+            dir = argv[1];
+        }
+        if (chdir(dir) != 0) { perror("cd"); rc = EXIT_FAILURE; } 
+        else { rc = EXIT_SUCCESS; }
+        return 1;
+    }
+    else if (strcmp(argv[0], "path") == 0) {
+        if (argc > 2) { wsh_warn(INVALID_PATH_USE); return 1; }
+        if (argc == 1) {
+            char* path = getenv("PATH");
+            if (path) printf("%s\n", path);
+        } else {
+            setenv("PATH", argv[1], 1);
+        }
+        rc = EXIT_SUCCESS;
+        return 1;
+    }
+    else if (strcmp(argv[0], "alias") == 0) {
+        if (argc == 1) { 
+            hm_print_sorted(alias_hm); 
+            rc = EXIT_SUCCESS;
+        } else if (argc >= 4 && strcmp(argv[2], "=") == 0) {
+            char* value = strdup(argv[3]);
+            for (int i = 4; i < argc; i++) {
+                value = append(value, " ");
+                value = append(value, argv[i]);
+            }
+            hm_put(alias_hm, argv[1], value); 
+            free(value);
+            rc = EXIT_SUCCESS;
+        } else { 
+            wsh_warn(INVALID_ALIAS_USE); 
+            return 1; 
+        }
+        return 1;
+    }
+    else if (strcmp(argv[0], "unalias") == 0) {
+        if (argc != 2) { wsh_warn(INVALID_UNALIAS_USE); return 1; }
+        hm_delete(alias_hm, argv[1]);
+        rc = EXIT_SUCCESS;
+        return 1;
+    }
+    else if (strcmp(argv[0], "which") == 0) {
+        if (argc != 2) { wsh_warn(INVALID_WHICH_USE); return 1; }
+        char* cmd = argv[1];
+        char* alias_val = hm_get(alias_hm, cmd);
+        if (alias_val) { 
+            printf(WHICH_ALIAS, cmd, alias_val); 
+            rc = EXIT_SUCCESS;
+            return 1; 
+        }
+        const char* builtins[] = {"exit", "cd", "path", "alias", "unalias", "which", "history", NULL};
+        for (int i = 0; builtins[i]; i++) {
+            if (strcmp(cmd, builtins[i]) == 0) {
+                printf(WHICH_BUILTIN, cmd);
+                rc = EXIT_SUCCESS;
+                return 1;
+            }
+        }
+        char* exec_path = find_executable(cmd);
+        if (exec_path) {
+            printf("%s\n", exec_path); 
+            free(exec_path);
+        } else {
+            printf(WHICH_NOT_FOUND, cmd);
+        }
+        rc = EXIT_SUCCESS;
+        return 1;
+    }
+    else if (strcmp(argv[0], "history") == 0) {
+        if (argc > 2) { wsh_warn(INVALID_HISTORY_USE); return 1; }
+        if (argc == 1) { 
+            for (size_t i = 0; i < history->size; i++) {
+                printf("%s\n", da_get(history, i));
+            }
+        } else {
+            char *endptr;
+            long n = strtol(argv[1], &endptr, 10);
+            if (*endptr != '\0' || n <= 0 || (size_t)n > history->size) {
+                wsh_warn(HISTORY_INVALID_ARG);
+                return 1;
+            } else {
+                printf("%s\n", da_get(history, n - 1));
+            }
+        }
+        rc = EXIT_SUCCESS;
+        return 1;
+    }
+    return 0;
+}
+
+char* find_executable(const char *cmd) {
+    if (strchr(cmd, '/') != NULL) { 
+        if (access(cmd, X_OK) == 0) return strdup(cmd);
+        return NULL;
+    }
+    
+    char *path_env = getenv("PATH");
+    if (!path_env || strlen(path_env) == 0) {
+        // Only warn if the command is not a built-in. This is a subtle point
+        // but prevents "PATH empty" messages for built-ins. We can't check
+        // for built-ins here easily, so the warning is acceptable as is.
+        wsh_warn(EMPTY_PATH);
+        return NULL;
+    }
+    
+    char *path_copy = strdup(path_env);
+    char *saveptr;
+    char *dir = strtok_r(path_copy, ":", &saveptr);
+    char full_path[MAX_LINE];
+    
+    while(dir) {
+        if (strlen(dir) > 0) {
+            snprintf(full_path, sizeof(full_path), "%s/%s", dir, cmd);
+            if (access(full_path, X_OK) == 0) {
+                free(path_copy);
+                return strdup(full_path);
+            }
+        }
+        dir = strtok_r(NULL, ":", &saveptr);
+    }
+    
+    free(path_copy);
+    return NULL;
+}
+
+void free_argv(int argc, char **argv) {
+    for (int i = 0; i < argc; i++) {
+        free(argv[i]);
+    }
+}
+
 
 /**
  * @Brief Parse a command line into arguments without doing
  * any alias substitutions.
  * Handles single quotes to allow spaces within arguments.
+ * (This is the provided function, unchanged).
  *
  * @param cmdline The command line to parse
  * @param argv Array to store the parsed arguments (must be preallocated)
@@ -188,14 +525,16 @@ void parseline_no_subst(const char *cmdline, char **argv, int *argc)
       *token = '\0';
       p = token + 1;
     }
-    argv[count] = strdup(token_start);
-    if (!argv[count]) {
-      perror("strdup");
-      for (int i = 0; i < count; i++) free(argv[i]);
-      free(buf);
-      clean_exit(EXIT_FAILURE);
+    if (count < MAX_ARGS - 1) {
+        argv[count] = strdup(token_start);
+        if (!argv[count]) {
+          perror("strdup");
+          for (int i = 0; i < count; i++) free(argv[i]);
+          free(buf);
+          clean_exit(EXIT_FAILURE);
+        }
+        count++;
     }
-    count++;
     while (*p && (*p == ' '))
       p++;
   }
